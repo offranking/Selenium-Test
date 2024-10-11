fc3e950798a20e7c24b6c316ad4f569be5235a7d
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2024-10-11 10:57:29 +0530

    Update TwitterAppApplicationTests.java

--- src/test/java/com/example/twitter_app/TwitterAppApplicationTests.java
+++ src/test/java/com/example/twitter_app/TwitterAppApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.twitter_app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-@SpringBootTest
-class TwitterAppApplicationTests {
+public class LoginTest {
 
-	@Test
-	void contextLoads() {
-	}
+    WebDriver driver;
 
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://example.com");
+    }
+
+    @Test
+    public void loginTest() {
+        System.out.println("Login Test executed");
+        // Add assertions and Selenium test code
+    }
 }
